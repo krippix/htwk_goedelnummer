61b36b3be901c5ccf466b05c52a554e2f9ebb75f
krippix <krippix@users.noreply.github.com>
2022-01-19 21:24:03 +0100

    prime number implementation finished

--- README.md
+++ README.md
@@ -10,7 +10,15 @@ Erstellt durch ***REMOVED***
 Beim Starten der Software wird zuerst eine instanz der Klasse PrimeNumber erstellt, 
 dabei wird dann die Datei "primes.dat" eingelesen.
 
+An die primes.dat werden die Folgenden Anforderungen gestellt:
+- Nur die Zahlen 0-9, getrennt durch Kommas
+- Aufsteigende Reihenfolge beginnend bei 2
+- Leerzeichen und Zeilenumbrüche werden ignoriert
+- Es müssen offensichlich Primzahlen sein
+
 ## Error codes
 Ich habe die Fehlecodes in etwa so verwendet, wie man es bei HTTP machen würde.
 
 - 404: Datei nicht gefunden
+- 416: Primzahl außerhalb des gültigen Bereichs 
+- 422: Datei enthält nicht verarbeitbare Zeichen
--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include "primeNumbers.hpp"
 
 int main(){
-
+    
+    PrimeNumbers prime; //Create PrimeNumber Object
+    
+    
+    
     //Attempts to create prime object, containing the primes imported from "primes.dat"
     try{
-        PrimeNumbers prime;
+        prime.init();
     }
-    catch (int customerror){
-        std::cout << "Exiting with error code <" << customerror << ">" << std::endl;
+    catch (int errorCode){
+        std::cout << "Exiting with error code <" << errorCode << ">" << std::endl;
         return EXIT_FAILURE;
     }
-    
+    for (int i = 0; i < prime.size(); i++){
+        std::cout << prime[i] << std::endl;
+    }
 }
